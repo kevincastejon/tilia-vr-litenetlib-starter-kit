46f937b81982692d77eced50eadb846e100a76f7
Kevin Castejon <kevincastejon@hotmail.fr>
2020-11-22 18:41:22 +0100

    added networked snapzone

---
+++ Assets/Scripts/Network/NetworkSnapZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tilia.Interactions.SnapZone;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SnapZoneFacade))]
+public class NetworkSnapZone : MonoBehaviour
+{
+    private SnapZoneFacade snapZone;
+    private void Start()
+    {
+        snapZone = GetComponent<SnapZoneFacade>();
+        if (!DEVNetworkSwitcher.isServer)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
