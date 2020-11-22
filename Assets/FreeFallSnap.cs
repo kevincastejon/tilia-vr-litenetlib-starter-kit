d20b3191552b49011e6275510449f5d395f3c6df
Kevin Castejon <kevincastejon@hotmail.fr>
2020-11-22 20:20:15 +0100

    added freefallsnap

---
+++ Assets/FreeFallSnap.cs
@@ -0,0 +1,20 @@
+﻿using Tilia.Interactions.Interactables.Interactables;
+using Tilia.Interactions.SnapZone;
+using UnityEngine;
+
+[RequireComponent(typeof(SnapZoneFacade))]
+public class FreeFallSnap : MonoBehaviour
+{
+    SnapZoneFacade snapZone;
+    private void Start()
+    {
+        snapZone = GetComponent<SnapZoneFacade>();
+    }
+    public void OnCollision(GameObject obj)
+    {
+        if (!obj.GetComponent<InteractableFacade>().IsGrabbed)
+        {
+            snapZone.Snap(obj);
+        }
+    }
+}
