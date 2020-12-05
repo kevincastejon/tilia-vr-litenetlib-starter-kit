77a552780ceb69ccf458672c5fcf6b1e6218743f
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-05 04:43:23 +0100

    big rollback

---
+++ Assets/Scripts/Misc/ColoredCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredCube : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private Color[] colors = new Color[] { Color.white, Color.black, Color.green, Color.yellow, Color.blue, Color.red };
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetRandomColor()
+    {
+        meshRenderer.material.color = colors[Random.Range(0, colors.Length)];
+    }
+}
