a16aef55a92b2e166e59dded93351c64760a16e6
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-09 03:36:18 +0100

    implement own oculus host and guest scripts

---
+++ Assets/Scripts/Network/Server/OculusClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Platform;
+using Oculus.Platform.Models;
+using UnityEngine;
+
+public class OculusClient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Core.Initialize();
+        Entitlements.IsUserEntitledToApplication().OnComplete(UserEntitled);
+        Matchmaking.SetMatchFoundNotificationCallback(RoomFound);
+        Rooms.SetUpdateNotificationCallback(RoomUpdated);
+    }
+
+    private void UserEntitled(Message msg)
+    {
+        if (!msg.IsError)
+        {
+            Debug.Log("You are entitled to use this app");
+            Users.GetLoggedInUser().OnComplete(OnUserInfo);
+        }
+        else
+        {
+            Debug.Log("error: You are not entitled to use this app");
+        }
+    }
+
+    private void OnUserInfo(Message<User> msg)
+    {
+        Debug.Log("LOGGED IN AS " + msg.Data.ID + " " + msg.Data.OculusID);
+        Matchmaking.Enqueue2("piootabouret");
+    }
+
+    private void RoomFound(Message<Room> msg)
+    {
+        RoomOptions opt = new RoomOptions();
+        Rooms.Join2(msg.Data.ID, opt).OnComplete(RoomJoined);
+    }
+
+    private void RoomJoined(Message<Room> msg)
+    {
+        Debug.Log("ROOM "+msg.Data.ID+" JOINED ");
+    }
+
+    private void RoomUpdated(Message<Room> msg)
+    {
+        Debug.Log("ROOM UPDATED");
+        for (int i = 0; i < msg.Data.UsersOptional.Count; i++)
+        {
+            User user = msg.Data.UsersOptional[i];
+            Debug.Log("- "+user.ID+" "+user.OculusID);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Network/Server/OculusServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Platform;
+using Oculus.Platform.Models;
+using UnityEngine;
+
+public class OculusServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Core.Initialize();
+        Entitlements.IsUserEntitledToApplication().OnComplete(UserEntitled);
+        Rooms.SetUpdateNotificationCallback(RoomUpdated);
+    }
+
+    private void UserEntitled(Message msg)
+    {
+        if (!msg.IsError)
+        {
+            Debug.Log("You are entitled to use this app");
+            Users.GetLoggedInUser().OnComplete(OnUserInfo);
+        }
+        else
+        {
+            Debug.Log("error: You are not entitled to use this app");
+        }
+    }
+
+    private void OnUserInfo(Message<User> msg)
+    {
+        Debug.Log("LOGGED IN AS " + msg.Data.ID + " " + msg.Data.OculusID);
+        Matchmaking.CreateAndEnqueueRoom2("piootabouret").OnComplete(RoomCreated);
+    }
+
+    private void RoomCreated(Message<MatchmakingEnqueueResultAndRoom> msg)
+    {
+        if (!msg.IsError)
+        {
+            Debug.Log("ROOM CREATED WITH ID "+ msg.Data.Room.ID);
+        }
+        else
+        {
+            Debug.Log("error: Failed to create and enqueue room - " + msg.GetError().Message);
+        }
+    }
+
+    private void RoomUpdated(Message<Room> msg)
+    {
+        Debug.Log("ROOM UPDATED");
+        for (int i = 0; i < msg.Data.UsersOptional.Count; i++)
+        {
+            User user = msg.Data.UsersOptional[i];
+            Debug.Log("- "+user.ID+" "+user.OculusID);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
