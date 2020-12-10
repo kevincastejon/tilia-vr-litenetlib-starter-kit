266d2bd5b820fc747c9248461cd1819b110e70d1
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-10 03:42:55 +0100

    switched to sbyte for playerstate id(cause server is -1)

--- Assets/Scripts/Network/Shared/Vector3Utils.cs
+++ Assets/Scripts/Network/Shared/Vector3Utils.cs
@@ -14,4 +14,16 @@ public class Vector3Utils
     {
         return new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
     }
+    public static void SerializeHand(NetDataWriter writer, Vector3 hand, Vector3 head)
+    {
+        Vector3 dif = hand - head;
+        writer.Put(dif.x / 2 * short.MaxValue);
+        writer.Put(dif.y / 2 * short.MaxValue);
+        writer.Put(dif.z / 2 * short.MaxValue);
+    }
+
+    public static Vector3 DeserializeHand(NetDataReader reader, Vector3 hand, Vector3 head)
+    {
+        return new Vector3(reader.GetShort() / short.MaxValue * 2, reader.GetShort() / short.MaxValue * 2, reader.GetShort() / short.MaxValue * 2);
+    }
 }
