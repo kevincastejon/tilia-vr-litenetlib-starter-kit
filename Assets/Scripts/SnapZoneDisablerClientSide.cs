930b49ffa081e3958b632e785be3949fce0537ed
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-10 19:06:44 +0100

    disabled SnapZone clientside

---
+++ Assets/Scripts/SnapZoneDisablerClientSide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapZoneDisablerClientSide : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!DEVNetworkSwitcher.isServer)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
