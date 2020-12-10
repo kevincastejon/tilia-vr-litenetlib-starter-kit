2241a88b019c62cf52ef1d948883e7a866698e37
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-10 04:04:04 +0100

    Update Vector3Utils.cs

--- Assets/Scripts/Network/Shared/Vector3Utils.cs
+++ Assets/Scripts/Network/Shared/Vector3Utils.cs
@@ -17,14 +17,14 @@ public class Vector3Utils
     public static void SerializeHand(NetDataWriter writer, Vector3 hand, Vector3 head)
     {
         Vector3 dif = hand - head;
-        writer.Put((short)(dif.x / 2 * short.MaxValue));
-        writer.Put((short)(dif.y / 2 * short.MaxValue));
-        writer.Put((short)(dif.z / 2 * short.MaxValue));
+        writer.Put((short)((dif.x / 2f) * short.MaxValue));
+        writer.Put((short)((dif.y / 2f) * short.MaxValue));
+        writer.Put((short)((dif.z / 2f) * short.MaxValue));
     }
 
     public static Vector3 DeserializeHand(NetDataReader reader, Vector3 head)
     {
-        Vector3 dif = new Vector3(reader.GetShort() / short.MaxValue * 2f, reader.GetShort() / short.MaxValue * 2f, reader.GetShort() / short.MaxValue * 2f);
+        Vector3 dif = new Vector3(((reader.GetShort() * 2f) / short.MaxValue), ((reader.GetShort() * 2f) / short.MaxValue), ((reader.GetShort() * 2f) / short.MaxValue));
         return head + dif;
     }
 }
