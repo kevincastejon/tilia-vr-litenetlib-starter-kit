b1b42408491972d96a5ca1ecdc81e7c847531296
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-13 01:18:43 +0100

    add back DisableCollisionOnGrab script

---
+++ Assets/Scripts/Extension_DisableCollisionsOnGrab/DisableCollisionsOnGrab.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tilia.Interactions.Interactables.Interactables;
+using Tilia.Interactions.Interactables.Interactors;
+using UnityEngine;
+
+[RequireComponent(typeof(InteractableFacade))]
+public class DisableCollisionsOnGrab : MonoBehaviour
+{
+    [Header("Reference settings")]
+    public List<Collider> colliders = new List<Collider>();
+    [Header("Toggle")]
+    public bool collidesOnGrab = false;
+    private InteractableFacade interactableFacade;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactableFacade = GetComponent<InteractableFacade>();
+        interactableFacade.Grabbed.AddListener(OnGrab);
+        interactableFacade.Ungrabbed.AddListener(OnUngrab);
+    }
+
+    private void OnGrab(InteractorFacade interactor)
+    {
+        if (!collidesOnGrab)
+        {
+            DisableColliders();
+        }
+    }
+
+    private void OnUngrab(InteractorFacade interactor)
+    {
+        EnableColliders();
+    }
+    public void DisableColliders()
+    {
+        foreach (Collider col in colliders)
+        {
+            col.isTrigger = true;
+        }
+    }
+    public void EnableColliders()
+    {
+        foreach (Collider col in colliders)
+        {
+            col.isTrigger = false;
+        }
+    }
+}
