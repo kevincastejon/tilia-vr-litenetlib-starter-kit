64bda9f2b07e277142c6ecf92e45933213bced67
Kevin Castejon <kevincastejon@hotmail.fr>
2020-12-28 21:21:04 +0100

    set the minimal MTU to ~1200 and implemented an autoselector for network dev

---
+++ Assets/DEV_NetworkAutoSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEV_NetworkAutoSelector : MonoBehaviour
+{
+    public Activable CreateButton;
+    public Activable JoinButton;
+    public bool isServer;
+    private void Awake()
+    {
+        if (isServer)
+        {
+            CreateButton.Activate();
+        }
+        else
+        {
+            JoinButton.Activate();
+        }
+    }
+}
